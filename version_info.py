93c73accfb0b7e298181239fedf9764541db75d6
MrBedrock <bumazhnietanki@gmail.com>
2026-06-20 17:07:59 +0700

    Add files via upload

---
+++ version_info.py
@@ -0,0 +1,32 @@
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(3, 0, 0, 0),
+    prodvers=(3, 0, 0, 0),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x40004,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+  ),
+  kids=[
+    StringFileInfo(
+      [
+        StringTable(
+          u'040904B0',
+          [
+            StringStruct(u'CompanyName', u'BedrockSFM & Limina Flow'),
+            StringStruct(u'FileDescription', u'SFM Community Patcher v3.0'),
+            StringStruct(u'FileVersion', u'3.0.0.0'),
+            StringStruct(u'InternalName', u'SFM_Patcher'),
+            StringStruct(u'OriginalFilename', u'SFM_Patcher_v3.exe'),
+            StringStruct(u'ProductName', u'SFM Community Patcher'),
+            StringStruct(u'ProductVersion', u'3.0.0.0'),
+            StringStruct(u'LegalCopyright', u'Copyright 2026 BedrockSFM & Limina Flow'),
+          ]
+        )
+      ]
+    ),
+    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+  ]
+)
